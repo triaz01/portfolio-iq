3b068aa719500348988791188a639b61a34a938b
triaz01 <tauseef.riaz@considracare.com>
2026-03-29 20:05:23 -0400

    fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   async rewrites() {
     return [
       {
